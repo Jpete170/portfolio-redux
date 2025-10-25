62497957c9c08aff66018c2e391e2e93d8c2aad4
Jackson Peters <jackson1480@hotmail.com>
2025-10-25 18:44:19 +1000

    add .env links

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,41 +1,49 @@
 <script setup lang="ts">
-  import type { NavigationMenuItem } from '@nuxt/ui';
-  const route = useRoute()
-
-  const routeList = computed<NavigationMenuItem[]>( ()=> [
-    {
-      label: 'About',
-      to: '/about',
-      active: route.path.startsWith("/about")
-    },
-    {
-      label: 'Projects',
-      to: '/projects',
-      active: route.path.startsWith("/projects")
-    },
-    {
-      label: 'Blog',
-      to: '/blog',
-      active: route.path.startsWith("/blog")
-    }
-  ])
-
+import type { NavigationMenuItem } from "@nuxt/ui";
+const route = useRoute();
+const config = useRuntimeConfig();
+const routeList = computed<NavigationMenuItem[]>(() => [
+  {
+    label: "About",
+    to: "/about",
+    active: route.path.startsWith("/about"),
+  },
+  {
+    label: "Projects",
+    to: "/projects",
+    active: route.path.startsWith("/projects"),
+  },
+  // {
+  //   label: 'Blog',
+  //   to: '/blog',
+  //   active: route.path.startsWith("/blog")
+  // }
+]);
 
 </script>
 <template>
   <UHeader title="JacksonPeters.dev">
+    <UNavigationMenu :items="routeList" />
 
-  <UNavigationMenu :items="routeList" />
-  
-  <template #right>
-    <UColorModeButton />
-    <div>
-      <p>GitHub</p>
-    </div>
-    <div>
-      <p>LinkedIn</p>
-    </div>
-  </template>
-  
+    <template #right>
+      <UColorModeButton size="xl"/>
+      <div>
+        <ULink
+          as="button"
+          type="button"
+          :to="config.public.GITHUB_PROFILE"
+          :external="true"
+          ><Icon name="uil:github" mode="svg" size="2em"/></ULink
+        >
+      </div>
+      <div>
+        <ULink
+          as="button"
+          :external="true"
+          :to="config.public.LINKEDIN_PROFILE"
+          ><Icon name="uil:linkedin" mode="svg" size="2em"/></ULink
+        >
+      </div>
+    </template>
   </UHeader>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/icon', '@nuxt/ui'],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  runtimeConfig: {
+    public:{
+      GITHUB_PROFILE: "",
+      LINKEDIN_PROFILE:""
+  }
+}
 })
