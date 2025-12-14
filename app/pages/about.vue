94dcf26e9e622e710d198cc965414a0ebff360ed
Jackson Peters <jackson1480@hotmail.com>
2025-12-14 16:33:35 +1000

    add skills

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,5 +1,50 @@
 <template>
-    <div>
-        <h1>About</h1>
+  <UPage>
+    <UPageHeader title="About Me" />
+    <div class="pt-5">
+      <p>
+        The page will act as a "living resume", where I will provide a
+        demonstration of skills that I've gained throughout my university
+        journey and self-study projects
+      </p>
     </div>
+    <UPageHeader title="Education" />
+    <UPageBody>
+      <div>
+        <p>
+          Bachelors of Information Technology, Queensland University of
+          Technology. 2018-2023
+        </p>
+      </div>
+      <div>
+        <p>
+          Certificate III in Information, Digital Media, and Technology, TAFE,
+          2016-2017
+        </p>
+      </div>
+    </UPageBody>
+    <UPageHeader title="Skills" />
+    <UPageBody>
+    <div class="p-1">
+      <p class="text-2xl pb-1">Programming Languages</p>
+    <p>I have used various programming languages, with a majority relating to web development.</p>
+    <p>In order of most experience to least:</p>
+    <ul class="list-disc list-inside">
+        <li>JavaScript</li>
+        <div class="px-10">
+            <li>React</li>
+            <li>React Native</li>
+            <li>Next</li>
+            <li>Nuxt</li>
+            <li>Express</li>
+        </div>
+        <li>Python</li>
+        <div class="px-10">
+            <li>Flask framework</li>
+        </div>
+    </ul>
+    
+    </div>
+</UPageBody>
+  </UPage>
 </template>
